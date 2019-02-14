6230581e0f0e45ecfbabf9857aaf13b47d559594
JoaoFSCruz <joao.fs.cruz97@gmail.com>
2019-02-14 21:23:50 +0000

    First implementation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,26 +7,22 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Ayo') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="{{ Route::currentRouteName() === 'welcome' ? 'homepage-background' : '' }}">
     <div id="app">
 
         @auth
             @include('partials.navbar')
         @endauth
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex-center position-ref full-height">
-        <div class="top-right links">
-            <button @click="$modal.show('login-modal')">Login</button>
+    <div class="flex py-10 px-16 h-screen">
+        <div class="bg-white rounded-lg shadow w-1/4 flex-col py-20 px-16 h-full">
+            <div class="mt-32">
+                <img src="/img/ayo-04.svg" alt="Ayo" class="w-32">
+            </div>
 
-            <button @click="$modal.show('register-modal')">Register</button>
-        </div>
+            <div class="mt-16 text-xl tracking-normal leading-normal" style="color: #979797;">
+                <p>Ayo helps you organize your time with mindful reminders, it's really simple.</p>
+                <p>You gotta try it dawg!</p>
+            </div>
 
-        <div class="content">
-            <div class="title m-b-md">
-                Ayo
+            <div class="flex-col pt-16">
+                <button class="w-full button-pink" @click="$modal.show('register-modal')">
+                    Get Started
+                </button>
+
+                <button class="w-full button-white mt-6" @click="$modal.show('login-modal')">
+                    Login
+                </button>
             </div>
         </div>
     </div>
