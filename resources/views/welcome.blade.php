754f71d3ca875c3b9ec66c40783479b2ba6e0a32
Guilherme Almeida <guicaricas2@gmail.com>
2019-02-15 22:51:44 +0000

    Responsive landing page tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex py-10 px-16 h-screen lg:justify-start sm:justify-center">
-        <div class="homepage-card lg:min-w-96 sm:min-w-1/2">
-            <div>
-                <img class="lg:w-32 sm:w-24" src="/img/ayo-04.svg" alt="Ayo">
-            </div>
-
-            <div class="homepage-card-text lg:text-xl sm:text-base lg:mt-16 sm:mt-8">
+<div class="flex py-10 px-16 h-screen lg:justify-start sm:justify-center">
+    <div class="flex flex-col justify-center bg-white max-w-xs rounded overflow-hidden shadow-lg px-6 py-6">
+        <img class="w-32" src="/img/ayo-04.svg" alt="Ayo">
+        <div class=" py-4">
+            <div class="homepage-card-text lg:text-xl sm:text-base">
                 <p>Ayo helps you organize your time with mindful reminders, it's really simple.</p>
                 <p>You gotta try it dawg!</p>
             </div>
+        </div>
+        <div class=" py-4">
+            <button class="w-full button-pink lg:py-5 sm:py-3" @click="$modal.show('register-modal')">
+                Get Started
+            </button>
 
-            <div class="flex-col lg:pt-16 sm:pt-8">
-                <button class="w-full button-pink lg:py-5 sm:py-3" @click="$modal.show('register-modal')">
-                    Get Started
-                </button>
-
-                <button class="w-full button-white mt-6 lg:py-5 sm:py-3" @click="$modal.show('login-modal')">
-                    Login
-                </button>
-            </div>
+            <button class="w-full button-white mt-6 lg:py-5 sm:py-3" @click="$modal.show('login-modal')">
+                Login
+            </button>
         </div>
     </div>
+</div>
 @endsection
