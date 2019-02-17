7e99777835eb25420b4f65ece96ea7b681adf34e
Guilherme Almeida <guicaricas2@gmail.com>
2019-02-17 22:28:24 +0000

    Initial styling

--- resources/js/components/RegisterModal.vue
+++ resources/js/components/RegisterModal.vue
@@ -1,77 +1,106 @@
 <template>
-    <modal name="register-modal">
-        <form method="POST" @submit.prevent="register" @keydown="feedback = ''">
-            <div>
-                <label for="username">Username</label>
+  <modal height="auto" :adaptive="true" name="register-modal">
+    <div class="flex justify-end">
+      <img class="p-4" src="/img/ic-close.svg" @click="$modal.hide('register-modal')"/>
+    </div>
+    <form
+      class="flex flex-col px-6 py-6"
+      method="POST"
+      @submit.prevent="register"
+      @keydown="feedback = ''"
+    >
+      <div class="text-left text-5xl font-black text-black">Welcome!</div>
+      <div
+        class="text-left text-xs text-grey-dark mb-6"
+      >Fill in the form in order to create an account.</div>
 
-                <div>
-                    <input id="username" type="text" name="username" v-model="form.username" autofocus>
-                </div>
-            </div>
+      <div>
+        <label class="label" for="username">Username</label>
+        <div>
+          <input
+            class="input mb-4 mt-2 w-full"
+            id="username"
+            type="text"
+            name="username"
+            v-model="form.username"
+            autofocus
+          >
+        </div>
+      </div>
 
-            <div>
-                <label for="email">E-Mail Address</label>
+      <div>
+        <label class="label" for="email">E-mail</label>
+        <div>
+          <input class="input mb-4 mt-2  w-full" id="email" type="email" name="email" v-model="form.email">
+        </div>
+      </div>
 
-                <div>
-                    <input id="email" type="email" name="email" v-model="form.email">
-                </div>
-            </div>
+      <div>
+        <label class="label" for="password">Password</label>
+        <div>
+          <input
+            class="input mb-4 mt-2 w-full"
+            id="password"
+            type="password"
+            name="password"
+            v-model="form.password"
+          >
+        </div>
+      </div>
 
-            <div>
-                <label for="password">Password</label>
+      <div>
+        <label class="label" for="password_confirmation">Password Confirmation</label>
+        <div>
+          <input
+            class="input mb-4 mt-2 w-full"
+            id="password_confirmation"
+            type="password"
+            name="password_confirmation"
+            v-model="form.password_confirmation"
+          >
+        </div>
+      </div>
 
-                <div>
-                    <input id="password" type="password" name="password" v-model="form.password">
-                </div>
-            </div>
+      <div>
+        <p v-text="feedback"></p>
+      </div>
 
-            <div>
-                <label for="password_confirmation">Password Confirmation</label>
-
-                <div>
-                    <input id="password_confirmation" type="password" name="password_confirmation" v-model="form.password_confirmation">
-                </div>
-            </div>
-
-            <div>
-                <p v-text="feedback"></p>
-            </div>
-
-            <div>
-                <button type="submit" :disabled="loading">Register</button>
-            </div>
-        </form>
-    </modal>
+      <div class="flex flex-row-reverse">
+        <button class="button button-pink w-1/3 mt-2" type="submit" :disabled="loading">Join us</button>
+      </div>
+    </form>
+  </modal>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                form: {
-                    username: '',
-                    email: '',
-                    password: '',
-                    password_confirmation: ''
-                },
-                loading: false,
-                feedback: ''
-            }
-        },
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        email: "",
+        password: "",
+        password_confirmation: ""
+      },
+      loading: false,
+      feedback: ""
+    };
+  },
 
-        methods: {
-            register() {
-                this.loading = true;
+  methods: {
+    register() {
+      this.loading = true;
 
-                axios.post('/register', this.form)
-                    .then(() => {
-                        location.assign('/home');
-                    })
-                    .catch(() => {
-                        this.loading = false;
-                        this.feedback = "Something went wrong, please try again.";
-                    })
-            }
-        }
+      axios
+        .post("/register", this.form)
+        .then(() => {
+          location.assign("/home");
+        })
+        .catch(() => {
+          this.loading = false;
+          this.feedback = "Something went wrong, please try again.";
+        });
     }
+  }
+};
 </script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="flex flex-row py-8 px-8 md:py-16 md:px-16 h-screen justify-center md:justify-start">
-    <div class="flex flex-col justify-center bg-white max-w-xs rounded-lg overflow-hidden shadow-lg px-12 py-6">
-        <img class="w-32 fill-current text-black" src="/img/ayo-04.svg" alt="Ayo">
+    <div class="flex flex-col justify-center bg-white max-w-xs rounded-sm overflow-hidden shadow-lg px-12 py-6">
+        <img class="w-32 text-black" src="/img/ayo-04.svg" alt="Ayo">
         <div class=" py-4">
             <div class="homepage-card-text text-sm">
                 <p>Ayo helps you organize your time with mindful reminders, it's really simple.</p>
