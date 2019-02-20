2ef84d80308d7bb8e3de75eaa71cb7b1f40f97cf
JoaoFSCruz <joao.fs.cruz97@gmail.com>
2019-02-20 15:55:58 +0000

    WIP register modal almost done

--- resources/js/components/RegisterModal.vue
+++ resources/js/components/RegisterModal.vue
@@ -3,92 +3,96 @@
         <div class="flex justify-end">
             <img class="p-2 cursor-pointer" src="/img/ic-close.svg" @click="$modal.hide('register-modal')"/>
         </div>
-        <form
-                class="flex flex-col px-6 py-6"
-                method="POST"
-                @submit.prevent="register"
-                @keydown="clear($event.target.name)"
-                novalidate
-                v-if="!registered"
-        >
-            <div class="text-left text-5xl font-black text-black">Welcome!</div>
-            <div class="text-left text-xs text-grey-dark mb-6">
-                Fill in the form in order to create an account.
-            </div>
-
-            <div>
-                <div class="flex justify-between">
-                    <label class="label" for="username">Username</label>
-                    <span v-if="errors.hasOwnProperty('username')" v-text="errors.username[0]" class="text-sm text-peachy-pink"></span>
+        <transition name="slide">
+            <form
+                    class="flex flex-col p-6"
+                    method="POST"
+                    @submit.prevent="register"
+                    @keydown="clear($event.target.name)"
+                    novalidate
+                    v-if="!registered"
+            >
+                <div class="text-left text-5xl font-black text-black">Welcome!</div>
+                <div class="text-left text-xs text-grey-dark mb-6">
+                    Fill in the form in order to create an account.
                 </div>
+
                 <div>
-                    <input
-                            class="input mb-4 mt-2 w-full"
-                            :class="{ 'border-2 border-peachy-pink' : errors.hasOwnProperty('username') }"
-                            id="username"
-                            type="text"
-                            name="username"
-                            v-model="form.username"
-                            autofocus
-                    >
+                    <div class="flex justify-between">
+                        <label class="label" for="username">Username</label>
+                        <span v-if="errors.hasOwnProperty('username')" v-text="errors.username[0]" class="text-sm text-peachy-pink"></span>
+                    </div>
+                    <div>
+                        <input
+                                class="input mb-4 mt-2 w-full"
+                                :class="{ 'border-2 border-peachy-pink' : errors.hasOwnProperty('username') }"
+                                id="username"
+                                type="text"
+                                name="username"
+                                v-model="form.username"
+                                autofocus
+                        >
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <div class="flex justify-between">
-                    <label class="label" for="email">E-mail</label>
-                    <span v-if="errors.hasOwnProperty('email')" v-text="errors.email[0]" class="text-sm text-peachy-pink"></span>
-                </div>
                 <div>
-                    <input
-                            class="input mb-4 mt-2 w-full"
-                            :class="{ 'border-2 border-peachy-pink' : errors.hasOwnProperty('email') }"
-                            id="email"
-                            type="email"
-                            name="email"
-                            v-model="form.email"
-                    >
+                    <div class="flex justify-between">
+                        <label class="label" for="email">E-mail</label>
+                        <span v-if="errors.hasOwnProperty('email')" v-text="errors.email[0]" class="text-sm text-peachy-pink"></span>
+                    </div>
+                    <div>
+                        <input
+                                class="input mb-4 mt-2 w-full"
+                                :class="{ 'border-2 border-peachy-pink' : errors.hasOwnProperty('email') }"
+                                id="email"
+                                type="email"
+                                name="email"
+                                v-model="form.email"
+                        >
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <div class="flex justify-between">
-                    <label class="label" for="password">Password</label>
-                    <span v-if="errors.hasOwnProperty('password')" v-text="errors.password[0]" class="text-sm text-peachy-pink"></span>
-                </div>
                 <div>
-                    <input
-                            class="input mb-4 mt-2 w-full"
-                            :class="{ 'border-2 border-peachy-pink' : errors.hasOwnProperty('password') }"
-                            id="password"
-                            type="password"
-                            name="password"
-                            v-model="form.password"
-                    >
+                    <div class="flex justify-between">
+                        <label class="label" for="password">Password</label>
+                        <span v-if="errors.hasOwnProperty('password')" v-text="errors.password[0]" class="text-sm text-peachy-pink"></span>
+                    </div>
+                    <div>
+                        <input
+                                class="input mb-4 mt-2 w-full"
+                                :class="{ 'border-2 border-peachy-pink' : errors.hasOwnProperty('password') }"
+                                id="password"
+                                type="password"
+                                name="password"
+                                v-model="form.password"
+                        >
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <label class="label" for="password_confirmation">Password Confirmation</label>
                 <div>
-                    <input
-                            class="input mb-4 mt-2 w-full"
-                            id="password_confirmation"
-                            type="password"
-                            name="password_confirmation"
-                            v-model="form.password_confirmation"
-                    >
+                    <label class="label" for="password_confirmation">Password Confirmation</label>
+                    <div>
+                        <input
+                                class="input mb-4 mt-2 w-full"
+                                id="password_confirmation"
+                                type="password"
+                                name="password_confirmation"
+                                v-model="form.password_confirmation"
+                        >
+                    </div>
                 </div>
-            </div>
 
-            <div class="flex flex-row-reverse">
-                <button class="button button-pink w-1/3 mt-2" type="submit" :disabled="submitDisabled">Join us</button>
-            </div>
-        </form>
+                <div class="flex flex-row-reverse">
+                    <button class="button button-pink w-1/3 mt-2" type="submit" :disabled="submitDisabled">Join us</button>
+                </div>
+            </form>
 
-        <div v-else>
-            Cenas
-        </div>
+            <div v-else class="flex flex-col items-center justify-center p-6">
+                <img class="w-32" src="/img/email-icon.svg" alt="Ayo">
+                <h2 class="text-center">Verify your account</h2>
+                <p class="text-xl text-center">A confirmation email was sent to your email address.</p>
+            </div>
+        </transition>
     </modal>
 </template>
 
@@ -103,7 +107,7 @@
                     password_confirmation: ''
                 },
                 loading: false,
-                errors: '',
+                errors: {},
                 registered: false
             };
         },
@@ -121,11 +125,9 @@
                 axios
                     .post('/register', this.form)
                     .then(() => {
-                        console.log("SUCCESS")
                         this.registered = true;
                     })
                     .catch((errors) => {
-                        console.log("FAIL")
                         this.loading = false;
                         this.errors = errors.response.data.errors;
                     });
@@ -137,3 +139,21 @@
         }
     };
 </script>
+
+<style>
+    .slide-leave-active,
+    .slide-enter-active {
+        transition: 1s;
+    }
+
+    .slide-enter {
+        transform: translate(100%, 0);
+        z-index: 1;
+    }
+
+    .slide-leave-to {
+        transform: translate(-100%, 0);
+        z-index: 0;
+        position: absolute;
+    }
+</style>
