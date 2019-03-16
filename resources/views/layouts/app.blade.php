084553983f02f56ada3c00ce00c62c7bd969e1db
Alexandre Almeida Olival <lex.olival@gmail.com>
2019-03-16 15:30:34 +0000

    Remove duplicate title tag on app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff8a80">
-    <meta name="msapplication-TileColor" content="#da532c"><title>{{ config('app.name', 'Ayo') }}</title>
+    <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ff8a80">
 
     <title>{{ config('app.name', 'Ayo') }}</title>
