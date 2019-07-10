a61a74e7ea6d79c01fbd2fedf57d53eb6fe92d14
JoaoFSCruz <joao.fs.cruz97@gmail.com>
2019-07-10 16:45:15 +0100

    menu shown when overed

--- resources/js/components/NavbarMenu.vue
+++ resources/js/components/NavbarMenu.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex flex-col">
-        <div class="flex flex-row items-center cursor-pointer" @click="toggleMenu">
+    <div class="flex flex-col" @mouseover="menuVisible = true" @mouseleave="menuVisible = false">
+        <div class="flex flex-row items-center cursor-pointer">
             <img class="h-8 w-8 rounded-full" src="https://cdn.flash.pt/images/2018-08/img_828x523$2018_08_22_11_45_38_153203_im_636786765242056305.png" alt="Avatar">
             <p class="ml-3 text-black">{{ user.username }}</p>
         </div>
-        <div slot="dropdown" class="bg-white shadow rounded border overflow-hidden absolute mt-10" v-show="menuVisible" v-cloak>
+        <div slot="dropdown" class="bg-white shadow rounded border overflow-hidden absolute mt-8" v-show="menuVisible" v-cloak>
             <a href="#" class="no-underline block px-8 py-2 border-b bg-white hover:text-white hover:bg-peachy-pink whitespace-no-wrap" @click="logout">Logout</a>
         </div>
         <form method="POST" action="/logout" class="hidden" id="logout-form">
