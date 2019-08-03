be9f3cac30d6422348c223fd42fb9821b479a374
João Cruz <joao.fs.cruz97@gmail.com>
2019-08-03 21:03:40 +0100

    Add overlay to avatar and create avatar upload component

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <div class="container mx-auto flex flex-col justify-center items-center h-full mt-10">
-        <img class="h-96 w-96 rounded-full border-8 border-peachy-pink" src="https://cdn.flash.pt/images/2018-08/img_828x523$2018_08_22_11_45_38_153203_im_636786765242056305.png" alt="Avatar">
-        <p class="text-4xl mt-6"> {{ auth()->user()->username }}</p>
-        <p class="text-grey-dark text-xl mt-6"> {{ auth()->user()->email }}</p>
+        <profile-picture></profile-picture>
+        <p class="text-3xl sm:text-4xl mt-6"> {{ auth()->user()->username }}</p>
+        <p class="text-grey-dark text-xl sm:text-xl mt-6"> {{ auth()->user()->email }}</p>
         <a class="button button-logout no-underline mt-10" href="{{ route('home') }}">Back</a>
     </div>
 @endsection
