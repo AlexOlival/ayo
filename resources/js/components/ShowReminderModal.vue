9d5c1bb4d6ccb0d75a339c951bb241ce51ad5319
João Cruz <joao.fs.cruz97@gmail.com>
2019-10-22 22:31:14 +0100

    WIP view reminder structure

--- resources/js/components/ShowReminderModal.vue
+++ resources/js/components/ShowReminderModal.vue
@@ -2,13 +2,31 @@
     <modal :classes="'rounded-lg bg-white max-w-1/2'" height="auto" :adaptive="true" name="show-reminder-modal" @before-open="beforeOpen">
         <div v-if="reminder">
             <div class="flex justify-end">
-                <img class="p-2 cursor-pointer" src="/img/ic-close.svg" @click="$modal.hide('show-reminder-modal')"/>
+                <img class="pr-8 pt-8 cursor-pointer" src="/img/ic-close.svg" @click="$modal.hide('show-reminder-modal')"/>
             </div>
-            <div class="flex flex-col px-6 py-6">
-                <div class="text-left text-4xl font-black text-black mb-1">{{ reminder.title }}</div>
-                <div class="text-left text-3xl text-black mb-8">{{ reminder.human_readable_notification_date }} <span class="text-2xl text-grey-dark">{{ reminder.notification_date }}</span></div>
-                <div>{{ reminder.description }}</div>
-                <div v-if="reminder.guests.length !== 0">HAS GUESTS</div>
+            <div class="flex flex-col px-10">
+                <div class="text-5xl text-black font-bold mb-4">{{ reminder.title }}</div>
+                <div class="flex flex-row mb-4">
+                    <span class="text-2xl mr-2">{{ reminder.human_readable_notification_date }}</span>
+                    <span class="text-2xl text-grey-dark">{{ reminder.notification_date }}</span>
+                </div>
+                <div class="mb-4" v-if="reminder.guests.length !== 0">
+                    <p class="text-grey-dark text-xl mb-4">People</p>
+                    <div class="flex mb-4" v-if="reminder.guests.length !== 0">
+                        <div class="flex bg-grey-lighter rounded-full w-36 items-center p-1 mr-2" v-for="guest in reminder.guests" :key="guest.id">
+                            <img class="h-10 w-10" :src="guest.avatar_path" alt="Avatar">
+                            <span class="truncate" v-text="guest.username"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col mb-6" v-if="reminder.description">
+                    <span class="text-grey-dark text-xl mb-2">Description</span>
+                    <span class="bg-grey-lighter rounded-xl p-2">{{ reminder.description }}</span>
+                </div>
+                <div class="flex justify-end">
+                    <button class="text-base text-grey-dark underline mr-2 p-6" >DELETE</button>
+                    <button class="text-base text-grey-dark underline p-6">EDIT</button>
+                </div>
             </div>
         </div>
     </modal>
