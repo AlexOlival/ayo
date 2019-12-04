34b46fbe0d1499577cb0779b8196a47df32c15bf
João Cruz <joao.fs.cruz97@gmail.com>
2019-12-04 17:36:30 +0000

    Small fixes

--- resources/js/components/ShowReminderModal.vue
+++ resources/js/components/ShowReminderModal.vue
@@ -57,7 +57,7 @@
     export default {
         data() {
             return {
-                reminder: null,
+                reminder: {},
                 showDeletePrompt: false
             }
         },
