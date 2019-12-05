068107cfe8b2a357f313de0cebaa7e89691c09c8
Alexandre Almeida Olival <lex.olival@gmail.com>
2019-12-05 22:33:42 +0000

    Style user profile modal

--- resources/js/components/ProfilePicture.vue
+++ resources/js/components/ProfilePicture.vue
@@ -1,17 +1,16 @@
 <template>
     <div class="flex flex-col items-center">
-        <div class="h-32 w-32 sm:h-32 sm:w-32 rounded-full border-4 sm:border-4 border-peachy-pink relative cursor-pointer"
+        <div class="h-32 w-32 sm:h-32 sm:w-32 rounded-full relative cursor-pointer flex justify-end"
              @click="openUploadPrompt">
-            <div class="ml-20 w-10 h-10 bg-peachy-pink absolute flex justify-end rounded">
-                <img class="h-4 w-4" src="/img/pencil-white.svg">
-            </div>
-            <img v-if="!hasNoAvatar" class="h-full w-full absolute" :src="avatar" alt="Avatar">
-            <div v-else>
-                <div class="h-full w-full absolute bg-peachy-pink rounded-full"></div>
-                <div class="h-full w-full flex flex-col justify-center items-center absolute">
+            <img v-if="!hasNoAvatar" class="h-full w-full absolute rounded-full border-4 sm:border-4 border-peachy-pink z-10" :src="avatar" alt="Avatar">
+            <div class="h-full w-full bg-peachy-pink rounded-full z-10" v-else>
+                <div class="h-full w-full flex flex-col justify-center items-center">
                     <p class="text-xs text-center text-white">Upload a new profile photo</p>
                     <p class="text-2xs text-center text-white">(max: 200px x 200px)</p>
                 </div>
+            </div>
+            <div class="h-16 w-16 bg-peachy-pink absolute flex justify-end rounded-lg p-1 z-0">
+                <img class="h-4 w-4" src="/img/pencil-white.svg">
             </div>
         </div>
 
@@ -27,15 +26,14 @@
     export default {
         computed: {
             hasNoAvatar() {
-                // let test = this.avatar.split('/');
-                // return test[test.length - 1] === 'default.svg';
-
-                return true
+                let test = this.avatar.split('/');
+                return test[test.length - 1] === 'default.svg';
             }
         },
 
         data() {
             return {
+                user: window.user,
                 avatar: window.user.avatar_path,
                 errors: {}
             }
