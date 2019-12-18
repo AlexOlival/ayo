5f87a938b0e4208d796102551f84f7ff56bbf15f
João Cruz <joao.fs.cruz97@gmail.com>
2019-12-18 22:59:10 +0000

    edit reminder finished

--- resources/js/components/ShowReminderModal.vue
+++ resources/js/components/ShowReminderModal.vue
@@ -1,11 +1,13 @@
 <template>
-    <modal :classes="'rounded-lg bg-white max-w-1/2'" height="auto" :adaptive="true" name="show-reminder-modal" @before-open="beforeOpen">
+    <modal :classes="'rounded-lg bg-white max-w-1/2'" height="auto" :adaptive="true" name="show-reminder-modal"
+           @before-open="beforeOpen">
         <div v-if="reminder">
             <div class="flex justify-end">
-                <img class="pr-8 pt-8 cursor-pointer" src="/img/ic-close.svg" @click="$modal.hide('show-reminder-modal')"/>
+                <img class="pr-8 pt-8 cursor-pointer" src="/img/ic-close.svg"
+                     @click="$modal.hide('show-reminder-modal')"/>
             </div>
-            <div class="flex flex-col px-10">
-                <div v-if="!showDeletePrompt">
+            <div class="flex flex-col p-6">
+                <div v-show="!showDeletePrompt && !showEditForm">
                     <div class="text-5xl text-black font-bold mb-4">{{ reminder.title }}</div>
                     <div class="flex flex-row mb-4">
                         <span class="text-2xl mr-2">{{ reminder.human_readable_notification_date }}</span>
@@ -14,7 +16,8 @@
                     <div class="mb-4" v-if="reminder.guests.length !== 0">
                         <p class="text-grey-dark text-xl mb-4">People</p>
                         <div class="flex mb-4" v-if="reminder.guests.length !== 0">
-                            <div class="flex bg-grey-lighter rounded-full w-36 items-center p-1 mr-2" v-for="guest in reminder.guests" :key="guest.id">
+                            <div class="flex bg-grey-lighter rounded-full w-36 items-center p-1 mr-2"
+                                 v-for="guest in reminder.guests" :key="guest.id">
                                 <img class="h-10 w-10" :src="guest.avatar_path" alt="Avatar">
                                 <span class="truncate" v-text="guest.username"></span>
                             </div>
@@ -29,18 +32,15 @@
                             <img class="cursor-pointer h-4 mr-1" src="/img/delete.svg"/>
                             <a class="text-sm font-semibold text-grey-dark underline mr-8 cursor-pointer">DELETE</a>
                         </div>
-                        <div class="flex items-center" @click="window.alert('edit')">
+                        <div class="flex items-center" @click="showEditForm = true">
                             <img class="cursor-pointer h-4 mr-1" src="/img/pencil.svg"/>
                             <a class="text-sm font-semibold text-grey-dark underline cursor-pointer">EDIT</a>
                         </div>
                     </div>
                 </div>
-                <div v-else class=" pb-6 mt-2">
-                    <p class="text-4xl text-black font-bold mb-4">Are you sure you want delete the reminder?</p>
-                    <p class="text-xl text-grey">
-                        The reminder to be deleted is
-                        <span class="font-semibold">{{ reminder.title }}</span>.
-                    </p>
+                <div v-show="showDeletePrompt && !showEditForm" class="">
+                    <p class="text-4xl text-black font-bold mb-4">Delete reminder</p>
+                    <p class="text-xl text-grey">Are you sure you want delete the reminder?</p>
                     <div class="flex justify-end mt-12">
                         <button class="rounded-full bg-grey-lighter text-black text-xl font-medium px-4 py-2 mr-4"
                                 @click="showDeletePrompt = false">Cancel</button>
@@ -48,24 +48,43 @@
                                 @click.prevent="deleteReminder()">Delete</button>
                     </div>
                 </div>
+                <div v-show="showEditForm">
+                    <reminder-form title="Edit reminder" :reminder="reminder" :errors="errors"
+                                   @closeEdit="showEditForm = false" @editReminder="editReminder"></reminder-form>
+                </div>
             </div>
         </div>
     </modal>
 </template>
 
 <script>
+
+    import ReminderForm from "./ReminderForm";
+
     export default {
+        components: {ReminderForm},
         data() {
             return {
-                reminder: {},
-                showDeletePrompt: false
+                reminder: null,
+                errors: {},
+                showDeletePrompt: false,
+                showEditForm: false
             }
         },
         methods: {
             beforeOpen(event) {
                 this.reminder = event.params.reminder;
             },
-
+            editReminder(reminder) {
+                reminder.notification_date += ':00';
+                axios.patch('/reminders/' + this.reminder.id, reminder)
+                    .then(() => {
+                        window.location.reload();
+                    })
+                    .catch((error) => {
+                        console.error(error.response.data);
+                    });
+            },
             deleteReminder() {
                 axios.delete('/reminders/' + this.reminder.id)
                     .then(() => {
@@ -74,6 +93,10 @@
                     .catch((error) => {
                         console.error(error.response.data);
                     });
+            },
+            clear() {
+                this.showEditForm = false;
+                this.showDeletePrompt = false;
             }
         }
     }
